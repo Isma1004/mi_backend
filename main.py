3cee8229b845fa56746928b3172804fd29cc9d59
Ismael Rodriguez <iscorodriguez99@gmail.com>
2026-08-12 15:57:12 -0600

    Le puse comentarios

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI  #se importa la libreria FastAPI
+
+app=FastAPI() #Se crea una instancia de la clase FastAPI (se crea un objeto de la clase FastAPI)
+
+@app.get("/") #Se crea la funcion para cuando se haga una peticion GET a la ruta raiz ("/")
+
+def raiz(): #funcion que FASTAPI ejecutara cuando se haga una peticion GET a la ruta raiz ("/")
+    return {"message": "Mi primer backend con FastAPI"} #Se retorna un diccionario con un mensaje de bienvenida
