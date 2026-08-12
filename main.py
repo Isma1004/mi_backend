b89485498b31fe391dd5471bb953229e072b2838
Ismael Rodriguez <iscorodriguez99@gmail.com>
2026-08-12 15:58:51 -0600

    Se actualizó el mensaje

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app=FastAPI() #Se crea una instancia de la clase FastAPI (se crea un objeto de l
 @app.get("/") #Se crea la funcion para cuando se haga una peticion GET a la ruta raiz ("/")
 
 def raiz(): #funcion que FASTAPI ejecutara cuando se haga una peticion GET a la ruta raiz ("/")
-    return {"message": "Mi primer backend con FastAPI"} #Se retorna un diccionario con un mensaje de bienvenida
+    return {"message": "Nuevo mensaje de bienvenida"} #Se retorna un diccionario con un mensaje de bienvenida
