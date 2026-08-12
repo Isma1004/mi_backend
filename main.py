0d32e9be11498904895c9c62f21d0ea068edefef
Ismael Rodriguez <iscorodriguez99@gmail.com>
2026-08-12 16:06:32 -0600

    cambuie el mensaje para verificar el reload

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app=FastAPI() #Se crea una instancia de la clase FastAPI (se crea un objeto de l
 @app.get("/") #Se crea la funcion para cuando se haga una peticion GET a la ruta raiz ("/")
 
 def raiz(): #funcion que FASTAPI ejecutara cuando se haga una peticion GET a la ruta raiz ("/")
-    return {"message": "Nuevo mensaje de bienvenida"} #Se retorna un diccionario con un mensaje de bienvenida
+    return {"message": "Probando el reload"} #Se retorna un diccionario con un mensaje de bienvenida
